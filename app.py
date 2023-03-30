06b2aa8d25322fc99ad6a9376439a9a96c05748f
Avichal Sharma <avichalsharma2003@gmail.com>
2023-03-30 14:41:19 +0000

    store application in db

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs, load_job
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs, load_job, add_app
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  jobs=load_jobs()
+  jobs = load_jobs()
   return render_template('home.html', jobs=jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(jobs)
 
+
 @app.route("/job/<id>")
 def show_job(id):
-  job=load_job(id)
+  job = load_job(id)
   if not job:
     return "Not found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job(id)
+  add_app(id, data)
+  return render_template('app_sub.html', application=data, job=job)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,34 +1,47 @@
 import os
 from sqlalchemy import create_engine, text
 
-db_conn_str=os.environ['DB_CONN_STR']
+db_conn_str = os.environ['DB_CONN_STR']
 
-engine = create_engine(
-  db_conn_str, 
-  connect_args={
-  "ssl": {
-    "ssl_ca": "/etc/ssl/cert.pem"
-  }
-})
+engine = create_engine(db_conn_str,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs():
   with engine.connect() as conn:
-    result=conn.execute(text("select * from jobs"))
-    col_names=result.keys()
-    jobs=[]
+    result = conn.execute(text("select * from jobs"))
+    col_names = result.keys()
+    jobs = []
     for row in result.all():
       jobs.append(dict(zip(col_names, row)))
     return jobs
 
+
 def load_job(id):
   with engine.connect() as conn:
-    result=conn.execute(
-      text(f"select * from jobs where id = {id}")
-    )
-    rows=[]
-    for row in result.all():
-      rows.append(row._mapping)
-    if len(rows)==0:
+    result = conn.execute(text(f"select * from jobs where id = {id}"))
+    rows = []
+    column_name = result.keys()
+    for rowi in result.all():
+      rows.append(dict(zip(column_name, rowi)))
+    if len(rows) == 0:
       return None
     else:
       return [dict(row) for row in rows]
+
+
+def add_app(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin, education, experience, resume) VALUES (:job_id, :full_name, :email, :linkedin, :education, :experience, :resume)"
+    )
+    conn.execute(query,
+                 job_id=job_id,
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin=data['linkedin'],
+                 education=data['education'],
+                 experience=data['experience'],
+                 resume=data['resume'])
